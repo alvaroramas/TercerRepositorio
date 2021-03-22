f31bfb6da729a6e13b861c3e0c2b929fd60f37d7
Alvaro <Alvaro@DESKTOP-SNU9D1N>
2021-03-22 13:25:12 +0100

    Nueva rama

--- HolaMundo/src/hola/mundo/HolaMundo.java
+++ HolaMundo/src/hola/mundo/HolaMundo.java
@@ -3,8 +3,7 @@ package hola.mundo;
 public class HolaMundo {
 
 	public static void main(String[] args) {
-		//AÃ±adiendo un comentario a la nueva funcionalidad
+		//Añadiendo un comentario a la nueva funcionalidad
 		System.out.println("Hola Mundo");
 	}
-
 }
